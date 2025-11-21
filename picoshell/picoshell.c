5ca8126ec21ea58cb33d1343a0cc3a6ea8f1762c
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-11-21 13:17:21 -0300

    Refactor picoshell function for better readability

--- picoshell/picoshell.c
+++ picoshell/picoshell.c
@@ -4,51 +4,52 @@
 
 int	picoshell(char **cmds[])
 {
-    int i = 0, fd[2], tmp_fd = 0;
-    pid_t pid;
+	int	i = 0, fd[2], tmp_fd = -1, pid;
 
-    while (cmds[i])
-    {
-        if (cmds[i + 1] && pipe(fd) == -1)
-        {
-            if (tmp_fd)
+	while (cmds[i])
+	{
+		if (cmds[i + 1] && pipe(fd) == -1)
+		{
+			if (tmp_fd)
 				close(tmp_fd);
-            return (1);
-        }
-        if ((pid = fork()) == -1)
-        {
-            if (tmp_fd) close(tmp_fd);
-            if (cmds[i + 1])
+			return (1);
+		}
+		if ((pid = fork()) == -1)
+		{
+			if (tmp_fd)
+				close(tmp_fd);
+			if (cmds[i + 1])
 			{
 				close(fd[0]);
 				close(fd[1]);
 			}
-            return (1);
-        }
-        if (pid == 0)
-        {
-            if (tmp_fd && dup2(tmp_fd, 0) == -1)
+			return (1);
+		}
+		if (pid == 0)
+		{
+			if (tmp_fd && dup2(tmp_fd, 0) == -1)
 				exit(1);
-            if (tmp_fd)
+			if (tmp_fd)
 				close(tmp_fd);
-            if (cmds[i + 1])
-            {
-                close(fd[0]);
-                if (dup2(fd[1], 1) == -1)
+			if (cmds[i + 1])
+			{
+				close(fd[0]);
+				if (dup2(fd[1], 1) == -1)
 					exit(1);
-                close(fd[1]);
-            }
-            execvp(cmds[i][0], cmds[i]);
-            exit(1);
-        }
-        if (tmp_fd) close(tmp_fd);
-        if (cmds[i + 1])
+				close(fd[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		if (tmp_fd)
+			close(tmp_fd);
+		if(cmds[i + 1])
 		{
 			close(fd[1]);
 			tmp_fd = fd[0];
 		}
-        i++;
-    }
-    while (wait(NULL) != -1);
-    return (0);
+		i++;
+	}
+	while (wait(NULL) != -1);
+	return (0);
 }
